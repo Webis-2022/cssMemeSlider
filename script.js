975fb83a1b1fef10f28635ba375d31d0bc64842e
Webis-2022 <verjovkin.jevgeni@gmail.com>
2024-09-14 08:54:48 +0300

    feat: change moveLeft function Сб, сен 14, 2024 08:54:48

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ let sliderText = document.querySelector('.slider-text');
 let btn2 = document.querySelector('.button2');
 let sliderControls = document.querySelectorAll('.slider-controls');
 let btnWrapper = document.querySelectorAll('.btn-wrapper');
-console.log(btnWrapper)
 let previousNumOfBtn = 1;
+let gap = slider.style.gap;
+let ruleIndex;
+let styleSheet;
 let arrOfImages = ["Did you do", "I did it", "Let's drink", "I don't drink"];
 let arrOfText = ["Did you do the Meme Slider task?", "Yes, I did it!", "Let's drink friend!", "No, until finishing Front-end Course I have a dry law"];
 
@@ -37,14 +39,53 @@ function deleteImg() {
     document.querySelectorAll('.slider img')[0].remove();
 }
 
-const buttons = document.querySelectorAll('.square, .btn');
+const buttons = document.querySelectorAll('.btn');
 let countIfButton1Pressed = 0;
-let countClicks = 0
+let countClicks = 0;
+
+const moveLeft = () => {
+    const sliderImg = document.querySelector('.slider img');
+        const slideWidth = sliderImg.offsetWidth;
+        slider.style.animation = `move-left 1.3s ease-in-out`;
+        let fullWidth = Number(slideWidth + gap) + 70;
+        styleSheet = document.styleSheets[0];
+        ruleIndex = styleSheet.insertRule(`
+            @keyframes move-left {
+                from {
+                    transform: translateX(0px);
+                }
+                to {
+                    transform: translateX(-${fullWidth}px);
+                }
+            }
+        `, styleSheet.cssRules.length);
+}
+
+// Disable buttons
+const disableButtons = () => {
+    buttons.forEach(button => button.setAttribute('disabled', true));
+};
+
+// Enable buttons
+const enableButtons = () => {
+    buttons.forEach(button => button.removeAttribute('disabled'));
+};
+function handleButtonClick(event) {
+    // Remove 'active' class from all buttons
+    buttons.forEach(button => {
+        button.classList.remove('active');
+    });
+    
+    // Add 'active' class to only pressed button
+    event.target.classList.add('active');
+}
+
 buttons.forEach(button => {
     button.addEventListener('click', (event)=>{
+        handleButtonClick(event);
         countClicks++;
-        if (event.target.classList.contains('button1') || event.target.classList.contains('btn-wrapper1')) {
-            countIfButton1Pressed;
+        if (event.target.classList.contains('button1')) {
+            countIfButton1Pressed++;
         }
         let buttonClass = event.target.classList[1];
         let numOfBtn = buttonClass.slice(-1);
@@ -52,22 +93,24 @@ buttons.forEach(button => {
             event.preventDefault();
         }
         else {
+        disableButtons();
         createImg(numOfBtn);
-        console.log('hi')
+        console.log('hi');
         sliderText.classList.add('hidden');
 
         setTimeout(() => {
             changeText(numOfBtn);
             sliderText.classList.remove('hidden');
-        }, 1000);
-        document.querySelector('.slider').classList.add('transition-left'); 
+            enableButtons();
+            }, 1300);
+        moveLeft();
         }
         previousNumOfBtn = numOfBtn;
     })
 })
 
 slider.addEventListener('animationend', ()=> {
-    
+    styleSheet.deleteRule(ruleIndex); 
     document.querySelector('.slider').classList.remove('transition-left');
     deleteImg();
 })
